21c84550658de18857f74a15d8fe2da5ed8cc7ca
prince jon <princejonc@gmail.com>
2018-08-09 02:01:03 +0800

    Add project files.

---
+++ WindowsFormsApplication3T/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tDES = new TripleDESCryptoServiceProvider();
+
+            tDES.Key = md5.ComputeHash(utf8.GetBytes(textBox1.Text));
+            tDES.Mode = CipherMode.ECB;
+            tDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = tDES.CreateEncryptor();
+
+            textBox3.Text = BitConverter.ToString(trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length));
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+
+    
+
